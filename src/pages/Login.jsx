b91c4a8aa3f33f6fd28644848c6ff42b6d26079e
Aditya Parmar <parmar.adi371@gmail.com>
2021-10-02 21:33:26 +0530

    login and register api intergration

---
+++ src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { NavLink, useHistory } from 'react-router-dom';
+
+function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = async(e)=>{
+    e.preventDefault();
+
+    const res = await fetch('https://peer-finder-be.herokuapp.com/login',{
+      method: "POST",
+      withCredentials: true,
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        email, password
+      })
+    });
+
+    const response = await res.json();
+    if(res.status !== 201){
+      window.alert(response.error);
+    }
+    else{
+      window.alert("Successful Login");
+      console.log("Profile set up nahi hui hai");
+      // console.log(response.tokens);
+      localStorage.setItem('token', response.tokens);
+      if(response.isProfileSetup === false){
+        history.push("/setprofile1");
+      }
+      else{
+        history.push("/");
+      }
+    }
+}
+
+  return (
+    <div className="sign_in_form">
+      <div className="sign_in_form_head">
+        <h1>Login to your Account</h1>
+      </div>
+      <form method="POST">
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter Your E-mail"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Enter Your Password"
+            value={password}
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={loginUser}>
+          Log In
+        </button>
+        <p className="create_account">
+          Does not have account? <NavLink to="/signup">Create account</NavLink>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,21 +1,17 @@
-import React,{useState} from 'react'
-import { FaAngleRight } from 'react-icons/fa'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './landing.css'
 
 function Landing() {
-    const [started, setstarted] = useState(false)
     return (
         <div className='landing-container'>
             <article>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo repudiandae natus officiis doloremque laboriosam modi tempora aspernatur neque vel. Nobis dignissimos sit quo doloremque magnam? Iusto sequi doloribus odio adipisci.</article>
             <div className="buttons">
             <h2>Start your journey here</h2>
-                {started && <> <button className="btn btn-lg btn-success">Login</button>
-                <button className="btn btn-lg btn-outline-dark">Create Account</button><br/></>}
-                {!started && <div className="start">
-                <button onClick={()=>setstarted(!started)}><FaAngleRight/></button>
-                </div>}
+            <Link to='/login'><button className="btn btn-lg btn-success">Login</button></Link>
+            <Link to='/register'><button className="btn btn-lg btn-outline-dark">Create Account</button></Link>
             </div>
-            <button className='btn btn-lg btn-outline-dark'>Take me home</button>
+            {/* <button className='btn btn-lg btn-outline-dark'>Take me home</button> */}
 
         </div>
     )
