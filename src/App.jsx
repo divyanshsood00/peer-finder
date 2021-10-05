8de1ba8433ec15a6d07328b02f5c0e0664292554
divyanshsood00 <divyanshsood00@gmail.com>
2021-10-05 14:52:55 +0530

    unfucking multiple fuckups

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
@@ -10,17 +10,27 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
+  const [active,setActive] = useState(false)
+  let currentURL = window.location.href
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    currentURL = window.location.href;
+    if (currentURL.endsWith('/home'))
+            setActive(true);
+  }, [currentURL])
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container-main">
           <div className="row">
-            <div className="col-lg-3 col-md-4">
+            {active && <div className="col-lg-3 col-md-4">
               <div className="app__sidebar">
                 <Sidebar/>              
               </div>
-            </div>
-            <div className="col-lg-9 col-md-8">
+            </div>}
+
+            {active && <div className="col-lg-9 col-md-8">
               <div className="app__main-content">
               <Navbar />
               <Switch>
@@ -47,7 +57,35 @@ function App() {
                 </Route>
               </Switch>
               </div>
-            </div>
+            </div>}
+            {!active && <div className="col">
+              <div className="app__main-content">
+              <Navbar />
+              <Switch>
+                <Route path='/' exact>
+                  <Landing/>
+                </Route>
+                <Route path='/home'>
+                  <Home/>
+                </Route>
+                <Route path='/courses'>
+                  <Courses/>
+                </Route>
+                <Route path='/notes'>
+                  <Notes/>
+                </Route>
+                <Route path='/login'>
+                  <Login/>
+                </Route>
+                <Route path='/register'>
+                  <Register/>
+                </Route>
+                <Route>
+                  <Redirect to='/'/>
+                </Route>
+              </Switch>
+              </div>
+            </div>}
           </div>
         </div>
       </BrowserRouter>
